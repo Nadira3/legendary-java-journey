1c95e9d5baf0659be5c3084b60c55b52e9f306db
Nadira3 <paitanun35@gmail.com>
2024-09-20 09:40:53 +0100

    (feat) => entry point and options implementation

---
+++ projects/simple_banking_application/BankingApplication.java
@@ -0,0 +1,20 @@
+class Options
+{
+	public Options()
+	{
+		System.out.println("Welcome to the Simple Banking Application!\nPlease select an option:\n1. Deposit Money\n2. Withdraw Money\n3. View Balance\n4. Exit");
+	}
+
+	public void takeResponse(int response)
+	{
+	}
+}
+
+public class BankingApplication
+{
+	public static void main(String args[])
+	{
+		int response;
+		Options option = new Options();
+	}
+}
