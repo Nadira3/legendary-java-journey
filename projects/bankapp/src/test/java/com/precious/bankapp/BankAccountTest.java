2ece342b5197c7473e21318f11e439782e239d06
Nadira3 <paitanun35@gmail.com>
2024-09-28 07:05:28 +0100

    (feat) => proper directory structure isolating multiple projects to use maven for unittesting

--- projects/bankapp/src/test/java/com/precious/bankapp/BankAccountTest.java
+++ projects/bankapp/src/test/java/com/precious/bankapp/BankAccountTest.java
@@ -1,14 +1,14 @@
 package com.precious.bankapp;
 
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BankAccountTest {
 
     private BankAccount account;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         // Set up an account with an initial balance of $100
         account = new BankAccount(100.0);
