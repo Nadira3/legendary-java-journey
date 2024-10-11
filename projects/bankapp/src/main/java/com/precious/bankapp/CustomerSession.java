e66ff62db341f6b281cce058cae31fbe46f49f66
Nadira3 <paitanun35@gmail.com>
2024-10-11 12:08:50 +0100

    proper code alignment

--- projects/bankapp/src/main/java/com/precious/bankapp/CustomerSession.java
+++ projects/bankapp/src/main/java/com/precious/bankapp/CustomerSession.java
@@ -6,56 +6,60 @@ import java.util.Scanner;
 /**
  * Class to manage a customer session after login.
  */
-public class CustomerSession {
+public class CustomerSession
+{
+	private Customer customer;
+    	private BankAction bankAction;
+    	private Scanner scanner; // Scanner passed from BankingApplication
+    	private CustomerService customerService;
 
-    private Customer customer;
-    private BankAction bankAction;
-    private Scanner scanner; // Scanner passed from BankingApplication
-    private CustomerService customerService;
+    	public CustomerSession(Customer customer, BankAction bankAction, Scanner scanner, CustomerService customerService)
+	{
+        	this.customer = customer;
+        	this.bankAction = bankAction;
+		this.customerService = customerService;
+        	this.scanner = scanner; // Using the shared scanner
+	}
 
-    public CustomerSession(Customer customer, BankAction bankAction, Scanner scanner, CustomerService customerService) {
-        this.customer = customer;
-        this.bankAction = bankAction;
-	this.customerService = customerService;
-        this.scanner = scanner; // Using the shared scanner
-    }
+    	// Start the session after login
+    	public void start()
+	{
+        	while (true)
+		{
+            		System.out.println("\nCustomer Menu for " + customer.getName() + ":");
+            		System.out.println("1. Create Account");
+            		System.out.println("2. Deposit");
+            		System.out.println("3. Withdraw");
+           	 	System.out.println("4. Transfer Funds");
+            		System.out.println("5. View Accounts");
+            		System.out.println("6. Log Out");
+            		int choice = InputUtils.getIntInput(scanner, "Enter choice: ");
 
-    // Start the session after login
-    public void start() {
-        while (true) {
-            System.out.println("\nCustomer Menu for " + customer.getName() + ":");
-            System.out.println("1. Create Account");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Transfer Funds");
-            System.out.println("5. View Accounts");
-            System.out.println("6. Log Out");
-            System.out.print("Enter your choice: ");
-            int choice = InputUtils.getIntInput(scanner, "Enter choice: ");
-
-            switch (choice) {
-                case 1:
-                    bankAction.createAccount(scanner, customer);
-                    break;
-                case 2:
-                    bankAction.performDeposit(scanner, customer);
-                    break;
-                case 3:
-                    bankAction.performWithdrawal(scanner, customer);
-                    break;
-                case 4:
-                    bankAction.performTransfer(scanner, customer, customerService);
-                    break;
-                case 5:
-                    bankAction.viewAccounts(customer);
-                    break;
-                case 6:
-		    customerService.updateCustomer(customer);
-                    System.out.println("Logging out...");
-                    return; // End session and go back to main menu
-                default:
-                    System.out.println("Invalid choice. Try again.");
-            }
-        }
-    }
+            		switch (choice)
+			{
+				case 1:
+					bankAction.createAccount(scanner, customer);
+                    			break;
+                		case 2:
+                    			bankAction.performDeposit(scanner, customer);
+                    			break;
+                		case 3:
+                    			bankAction.performWithdrawal(scanner, customer);
+                    			break;
+                		case 4:
+                    			bankAction.performTransfer(scanner, customer, customerService);
+                    			break;
+                		case 5:
+                    			bankAction.viewAccounts(customer);
+                    			break;
+                		case 6:
+		    			customerService.updateCustomer(customer);
+                    			System.out.println("Logging out...");
+                    			System.out.println();
+                    			return; // End session and go back to main menu
+                		default:
+                    			System.out.println("Invalid choice. Try again.");
+            		}
+        	}
+    	}
 }
