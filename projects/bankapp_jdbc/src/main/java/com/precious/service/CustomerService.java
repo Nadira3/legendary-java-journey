f318ccac59d702b1593c3999f179bcc49fd57fc3
Nadira3 <paitanun35@gmail.com>
2024-10-16 10:54:57 +0100

    new features in customerservice class

--- projects/bankapp_jdbc/src/main/java/com/precious/service/CustomerService.java
+++ projects/bankapp_jdbc/src/main/java/com/precious/service/CustomerService.java
@@ -19,10 +19,34 @@ public class CustomerService {
     public CustomerService(DataBaseUtils dbUtils) {
 	    this.customerDAO = new CustomerDAO(dbUtils);
     }
+
+    public Customer createCustomer(Scanner scanner) {
+    
+	    System.out.print("Enter your name: ");
+	    String name = scanner.nextLine();
+    
+	    System.out.print("Enter your address: ");
+	    String address = scanner.nextLine();
+    
+	    // Generate a unique customer ID using the UniqueIDGenerator class
+	    String customerId = UniqueIDGenerator.generateAlphanumericId("CUST", 6);
+	    // Create a new customer object
+	    Customer newCustomer = new Customer(name, customerId, address);
+    
+	    // Add the new customer to the system
+	    customerDAO.addCustomer(newCustomer);
+    
+	    // Output success message
+	    System.out.println("Customer created successfully.");
+	    System.out.println("Your Customer ID is: " + newCustomer.getId());
+
+        return newCustomer;
+	}
+    
     // Method to retrieve a customer by their ID
     public Customer getCustomerByID(String customerID) {
-	Customer customer = customerDAO.getCustomerById(customerID);
-	if (customer == null)
+	    Customer customer = customerDAO.getCustomerById(customerID);
+	    if (customer == null)
         // If no customer is found, return null or handle the case as needed
 	        System.out.println("No customer found with ID: " + customerID);
         return customer;
@@ -30,8 +54,8 @@ public class CustomerService {
 
     // Method to retrieve a customer by their account number
     public Customer getCustomerByAccountNumber(String accountNumber) {
-	Customer customer = customerDAO.getCustomerByAccountNumber(accountNumber);
-	if (customer == null)
+	    Customer customer = customerDAO.getCustomerByAccountNumber(accountNumber);
+	    if (customer == null)
         // If no customer is found, return null or handle the case as needed
 	        System.out.println("No customer found with account number: " + accountNumber);
         return customer;
@@ -40,13 +64,13 @@ public class CustomerService {
 
     public void addCustomer(Customer customer) {
         // Read the current list of customers from the database
-	customerDAO.addCustomer(customer);
+	    customerDAO.addCustomer(customer);
         System.out.println("Customer added successfully.");
     }
 
     public void updateCustomer(Customer updatedCustomer) {
         // Read the current list of customers from the database
-	customerDAO.updateCustomer(updatedCustomer);
+	    customerDAO.updateCustomer(updatedCustomer);
         System.out.println("Customer updated successfully.");
     }
 } 
