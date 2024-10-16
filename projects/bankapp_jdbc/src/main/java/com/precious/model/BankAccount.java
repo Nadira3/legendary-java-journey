6cf438b6ca4b13f283d2cdd87edfa9bb3f12fc44
Nadira3 <paitanun35@gmail.com>
2024-10-16 10:51:27 +0100

    new features in bank account class

--- projects/bankapp_jdbc/src/main/java/com/precious/model/BankAccount.java
+++ projects/bankapp_jdbc/src/main/java/com/precious/model/BankAccount.java
@@ -8,18 +8,30 @@ import java.util.Scanner;
 public abstract class BankAccount {
     protected double balance;
     protected String accountNumber;
+    protected String customerId;
+    protected int typeId;
 
-    public BankAccount(String accountNumber, double initialBalance) {
+    public BankAccount(String accountNumber, double initialBalance, String customerId, int typeId) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.customerId = customerId;
+        this.typeId = typeId;
     }
 
     public String getAccountNumber() {
         return accountNumber;
     }
 
+    public String getCustomerId() {
+        return customerId;
+    }
+
     public double getBalance() {
         return balance;
+    }
+
+    public int getTypeId() {
+        return typeId;
     }
 
     public abstract void deposit(double amount);
