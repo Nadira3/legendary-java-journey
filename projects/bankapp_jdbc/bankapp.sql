52224b8da877daf654f7c4801e48a1a8c50caae6
Nadira3 <paitanun35@gmail.com>
2024-10-16 10:48:52 +0100

    addded typeId to bankaccount table

--- projects/bankapp_jdbc/bankapp.sql
+++ projects/bankapp_jdbc/bankapp.sql
@@ -1,4 +1,4 @@
--- Create the bankapp database if it doesn't exist
+-- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS bankapp;
 
 -- Use the bankapp database
@@ -16,6 +16,13 @@ CREATE TABLE IF NOT EXISTS BankAccount (
     id INT AUTO_INCREMENT PRIMARY KEY,
     customerId VARCHAR(255),
     accountNumber VARCHAR(255),
+    typeID INT,
     balance DECIMAL(10, 2),
     FOREIGN KEY (customerId) REFERENCES Customer(id) ON DELETE CASCADE
+);
+
+-- Create the AccountType table if it doesn't exist
+CREATE TABLE IF NOT EXISTS AccountType (
+    typeID INT AUTO_INCREMENT PRIMARY KEY,
+    typeName VARCHAR(255) NOT NULL
 );
