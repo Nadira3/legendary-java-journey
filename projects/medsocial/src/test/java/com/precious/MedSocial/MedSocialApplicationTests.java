1b66d086d3bad958bd98bcfcd7e95e74aebf2089
Nadira3 <paitanun35@gmail.com>
2025-01-31 02:56:28 +0100

    chore(api-gateway): initial setup of API Gateway for medSocial microservices architecture

---
+++ projects/medsocial/src/test/java/com/precious/MedSocial/MedSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precious.MedSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
