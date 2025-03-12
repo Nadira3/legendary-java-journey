07089c8921baff533c821fc91b103be54e0af8bc
Nadira3 <paitanun35@gmail.com>
2025-03-12 07:18:42 +0100

    print combination with rules task

--- alx-projects/0x01-variables_if_else_while/100-print_comb3.java
+++ alx-projects/0x01-variables_if_else_while/100-print_comb3.java
@@ -10,3 +10,40 @@
  *
  * All your code should be in the main function
  */
+
+class PrintComb3 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = i + 1; j < 10; j++) {
+				System.out.print(i);
+				System.out.print(j);
+				if (i != 8 || j != 9) {
+					System.out.print(", ");
+				}
+			}
+		}
+		System.out.print("\n");
+	}
+}
+
+// java style
+class PrintComb3 {
+    public static void main(String[] args) {
+        printCombinations();
+    }
+
+    static void printCombinations() {
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < 10; i++) {
+            for (int j = i + 1; j < 10; j++) {
+                result.append(i).append(j);
+                if (i != 8 || j != 9) {
+                    result.append(", ");
+                }
+            }
+        }
+        
+        System.out.println(result);
+    }
+}
